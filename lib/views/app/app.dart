e1e78d6d68fae40640fe1951e9bb813a1bf56b6d
Benjamin McLemore <51517288+Chionophile@users.noreply.github.com>
2024-06-19 22:06:42 -0400

    noto sans theme

--- lib/views/app/app.dart
+++ lib/views/app/app.dart
@@ -2,6 +2,7 @@ import 'package:basic_flashcards/blocs/data/collections/collections_bloc.dart';
 import 'package:basic_flashcards/views/screens/home/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -14,6 +15,7 @@ class App extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlueAccent),
         useMaterial3: true,
+        textTheme: GoogleFonts.notoSansTextTheme(),
       ),
       home: MultiBlocProvider(
         providers: [
