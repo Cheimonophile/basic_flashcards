81aa95015d654a0c2fe1c1fd689ebb508104fbad
Benjamin McLemore <51517288+Chionophile@users.noreply.github.com>
2024-06-30 10:22:49 -0400

    Create decks with their name

--- lib/widgets/screens/deck/new_deck_screen.dart
+++ lib/widgets/screens/deck/new_deck_screen.dart
@@ -2,20 +2,25 @@ import 'package:basic_flashcards/blocs/data/decks/decks_bloc.dart';
 import 'package:basic_flashcards/interfaces/widgets/screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:uuid/uuid.dart';
 
 /// screen for creating a new deck
 class NewDeckScreen extends Screen {
   /// The decks bloc required for creating a new deck
   final DecksBloc decksBloc;
 
-  const NewDeckScreen({super.key, required this.decksBloc});
+  /// text editing controller for the name of the deck
+  final TextEditingController nameController = TextEditingController();
+
+  /// constructs a new deck screen
+  ///
+  /// [decksBloc] is required for creating a new deck
+  NewDeckScreen({super.key, required this.decksBloc});
 
   /// when the create deck button is pressed
   _onPressedCreateDeck(BuildContext context) {
-    final deckName = const Uuid().v4();
+    final name = nameController.text;
     BlocProvider.of<DecksBloc>(context).add(DecksCreate(
-      name: deckName,
+      name: name,
     ));
     Navigator.of(context).pop();
   }
@@ -32,10 +37,26 @@ class NewDeckScreen extends Screen {
             backgroundColor: Theme.of(context).colorScheme.inversePrimary,
             title: const Text("New Deck"),
           ),
-          body: Center(
-            child: ElevatedButton(
-              onPressed: () => _onPressedCreateDeck(context),
-              child: const Text("Create Deck"),
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Name of the deck
+                TextField(
+                  controller: nameController,
+                  decoration: const InputDecoration(
+                    labelText: "Name",
+                  ),
+                ),
+                const SizedBox(height: 8),
+
+                // create button
+                ElevatedButton(
+                  onPressed: () => _onPressedCreateDeck(context),
+                  child: const Text("Create Deck"),
+                ),
+              ],
             ),
           ),
         );
